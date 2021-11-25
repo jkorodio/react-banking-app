980e941b04628ef29488604f3337c542d792cff3
unknown <jkorodio.jobs@gmail.com>
2021-11-25 23:27:58 +0800

    create header w/ nav

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="header-left">
+        <img
+          src="https://www.freepnglogos.com/uploads/f-e-letter-logo-image-6.png"
+          alt=""
+        />{" "}
+        <h2>BANK OF ETIVAC</h2>
+      </div>
+      <div className="header-right-container">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/customer">Customer</Link>
+          </li>
+          <li>
+            <Link to="/transaction">Transaction</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Transaction.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Transaction = () => {
+  return (
+    <div>
+      <h1>TRANSACTION PAGE</h1>
+    </div>
+  );
+};
+
+export default Transaction;
