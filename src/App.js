8820194b5552ea947c0f683134b0df064c2df6c6
unknown <jkorodio.jobs@gmail.com>
2021-11-30 20:01:30 +0800

    add features

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Customer from "./pages/Customer";
+import Features from "./pages/Features";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Transaction from "./pages/transaction/Transaction";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/customer" element={<Customer />} />
         <Route path="/transaction" element={<Transaction />} />
+        <Route path="/features" element={<Features />} />
       </Routes>
     </BrowserRouter>
   );
